a153f3329ae7f9709fa163bce92d993423f2c275
Shushant-Priyadarshi <profshushant@gmail.com>
2024-12-09 21:37:05 +0530

    strings

---
+++ core/datatypes/string.js
@@ -0,0 +1,30 @@
+const learning = new String("Hello");
+const anotherStr = " World!"
+// console.log(learning.bold()); bolds the string
+// console.log(learning.charAt(2));
+//  console.log(learning.concat(" World!"));
+
+//  console.log(learning.endsWith("O")); Case sensitive
+
+//console.log(learning.includes("L"));  case sensitive
+
+// console.log(learning.indexOf("L")); case sensitive
+
+// const crush = "IDK"
+// const para = `I think i am in love with ${crush}`
+
+// console.log(para.lastIndexOf(crush));
+
+// console.log(learning.link("https://vercle.com")); for anchor tag
+
+// console.log(anotherStr);
+// console.log(anotherStr.trim());
+
+console.log(learning.match(anotherStr));
+
+
+
+
+
+
+
