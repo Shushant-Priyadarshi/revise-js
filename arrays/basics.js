8d3a5ff16fdf6f28f5b5fe18043e94d119eb1922
Shushant-Priyadarshi <profshushant@gmail.com>
2024-12-26 18:30:51 +0530

    started revising arrays

--- arrays/basics.js
+++ arrays/basics.js
@@ -1,5 +1,6 @@
+// An array is an object with multiple items in single variable
+
 const arr = [23,434,554,32];
-for(let i=0 ;i<arr.length ;i++){
-    console.log(arr[i]);
-    
-}
+
+
+console.log(typeof arr);
