7e9698a72de22acc61ae43de7017f0a567c63dc2
Shushant-Priyadarshi <profshushant@gmail.com>
2024-12-27 22:47:29 +0530

    learned objects

---
+++ objects/basic.js
@@ -0,0 +1,19 @@
+//object literal
+const obj = {
+    name:"Shu"
+}
+// console.log(obj["name"]);
+// console.log(obj.name);
+
+
+const users = {
+    name:"Shsuhat",
+    email:"Shu@gmail.com",
+    pass:"shu"
+}
+console.log(Object.values(users));
+Object.keys(users).map((item)=>(console.log(item)));
+
+
+
+
