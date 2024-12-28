259cc48c024158d6f073d71f23faed7de89e93ba
Shushant-Priyadarshi <profshushant@gmail.com>
2024-12-28 17:01:54 +0530

    revisited objects

---
+++ objects/json.js
@@ -0,0 +1,16 @@
+//++++++++++++++++++++++++++++++ Object Destructor +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+const user = {
+    userName:"Harry",
+    email:"harry@google.com",
+    password:"2123213SDSA!@#"
+}
+const { userName:name , email } = user
+console.log(`${name}'s email is : ${email}`);
+
+//++++++++++++++++++++++++++++++ APIs and JSON +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// {
+//     "uName":"shu",
+//     "uEmail":"shu@gmail.com"
+// }
