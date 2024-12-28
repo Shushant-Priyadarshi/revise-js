4746c0048fe8a3d0c976df2f44fa326ab5c9e9c6
Shushant-Priyadarshi <profshushant@gmail.com>
2024-12-28 17:33:58 +0530

    revisited rest and reduce

--- 04_funtions/funtions.js
+++ 04_funtions/funtions.js
@@ -1,3 +1,4 @@
+//++++++++++++++++++++++++++++++++++++ BASICS ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 const sayHello = ()=>{
     console.log("Hello");
     
@@ -8,4 +9,28 @@ function HelloWorld (){
     
 }
 
-sayHello();HelloWorld();
+// sayHello();
+// console.log(HelloWorld);
+ 
+//++++++++++++++++++++++++++++++++++++ Parameters and Arguments ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function add (n1,n2){ //Parameter
+    console.log(n1+n2);
+    
+}
+
+// add(3,4); //argument
+
+
+//++++++++++++++++++++++++++++++++++++ Rest Operator ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+function giveAll(...n){
+    return n;
+}
+
+const result = giveAll(1,2,3,4,5,6,7)
+const sum = result.reduce((total,res)=>total+res,0) // 0 is the initial value of total
+// console.log(`The sum of ${result} is : ${sum}`);
+
+//++++++++++++++++++++++++++++++++++++ Scopes ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
