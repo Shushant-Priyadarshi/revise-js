cf10d541f2e86a9f283982a0fc941d793e0799d9
Shushant <shushant@Shushants-MacBook-Air.local>
2025-01-02 23:34:06 +0530

    revisited scopes

---
+++ 04_funtions/scopes.js
@@ -0,0 +1,15 @@
+console.log("Learning Scopes");
+
+
+
+if(true){
+    let a =12;
+    const b=2;
+    var c=3; //var does not have block scope
+}
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+
+
