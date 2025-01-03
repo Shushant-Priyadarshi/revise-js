32c52c81bdefce9f0669c9eee73449c304714048
Shushant <shushant@Shushants-MacBook-Air.local>
2025-01-03 23:35:05 +0530

    learned hoisting in detailed

--- 04_funtions/hoisting.js
+++ 04_funtions/hoisting.js
@@ -1,2 +1,14 @@
 console.log("Hoisting.....");
 
+console.log( addOne(3));
+function addOne(num) {
+    return num + 1;
+}
+
+
+
+console.log(addTwio(3)); // Error
+const addTwio = function(num) {
+    return num +2 ;
+}
+
