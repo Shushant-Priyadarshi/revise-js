e6e896ed5b9c13920233de7e726419151162b31a
Shushant <shushant@Shushants-MacBook-Air.local>
2025-01-04 17:32:08 +0530

    learned execution context and call stacks

--- 04_funtions/iife.js
+++ 04_funtions/iife.js
@@ -1,12 +1,10 @@
 // IIFE: Immedietely Involked Function Expressions
 
-
-//Why need ?: Avoid Polluting the Global Scope
-// Variables defined within an IIFE are not accessible from the outside 
-// because they are confined to the function's scope.
+// Why need ?: Avoid Polluting the Global Scope
+// Variables defined within an IIFE are not accessible from the outside because they are confined to the function's scope.
 // This prevents naming collisions and keeps the global namespace clean.
 (function() {
-    var message = "Hello, World!";
+     message = "Hello, World!";
     console.log(message);
 })();
 
