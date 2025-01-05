d3dd5fa17668e635a77a8712a0a4f2eff22eb967
Shushant <shushant@Shushants-MacBook-Air.local>
2025-01-05 20:18:19 +0530

    revisited creating elements

---
+++ 07_DOM/02_creatingNewElements/one.js
@@ -0,0 +1,14 @@
+const weekdays = document.querySelector(".parent");
+ console.log(weekdays.children);
+ console.log(weekdays.children[1].innerText);
+
+for(let i =0; i < weekdays.children.length; i++){
+   // console.log(weekdays.children[i].innerHTML);
+    
+}
+
+console.log(weekdays.firstElementChild);
+
+console.log("NODES: ",weekdays.childNodes);
+
+
